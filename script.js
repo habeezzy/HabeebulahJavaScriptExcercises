317b376a9e7f8c823462ab29345ebed90fda8926
habeezzy <mhabeebulah@gmail.com>
2017-10-13 08:57:23 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var name = prompt('Enter a Number');
+alert(name);
+
+for (var name = 1; name<10; name++) {
+  total = total + name
+}console.log(total);
+
+
+var i = prompt("do you want to play");
+alert(i);
